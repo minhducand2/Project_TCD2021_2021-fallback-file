0c8b4d518fa38cd1729805670bfb9c63ebddbb8c
Phạm Văn Minh Đức <55387191+minhducand2@users.noreply.github.com>
2021-11-29 20:31:46 +0700

    Update login-cookie.ts

--- frontend-user/src/app/common/core/login-cookie.ts
+++ frontend-user/src/app/common/core/login-cookie.ts
@@ -5,27 +5,6 @@ import { Injectable } from '@angular/core';
 export class LoginCookie {
     public accountLogin: string = '0';
 
-    // constructor(
-    //     private cookieService: CookieService
-    // ) {
 
-    // }
-
-    // /**
-    //  * setAccountLogin
-    //  * @param value 
-    //  */
-    // public setAccountLogin(value: string) {
-    //     this.cookieService.set('accountLogin', value);
-    //     this.accountLogin = value;
-    // }
-
-    // /**
-    //  * getAccountLogin
-    //  */
-    // public getAccountLogin(): string {
-    //     this.accountLogin = this.cookieService.get('accountLogin');
-    //     return this.accountLogin;
-    // } 
 
 }
