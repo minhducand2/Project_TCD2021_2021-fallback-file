275cde8a9dafb58f8e653534225cf78c46e5fcba
Phạm Văn Minh Đức <55387191+minhducand2@users.noreply.github.com>
2021-11-29 20:49:44 +0700

    Update inline.js

--- frontend-admin/src/assets/ckeditor/core/creators/inline.js
+++ frontend-admin/src/assets/ckeditor/core/creators/inline.js
@@ -1,33 +1,9 @@
-/**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
+
 
 ( function() {
 	/** @class CKEDITOR */
 
-	/**
-	 * Turns a DOM element with the `contenteditable` attribute set to `true` into a
-	 * CKEditor instance. Check {@link CKEDITOR.dtd#$editable} for a list of
-	 * allowed element names.
-	 *
-	 * **Note:** If the DOM element for which inline editing is being enabled does not have
-	 * the `contenteditable` attribute set to `true` or the {@link CKEDITOR.config#readOnly `config.readOnly`}
-	 * configuration option is set to `false`, the editor will start in read-only mode.
-	 *
-	 *		<div contenteditable="true" id="content">...</div>
-	 *		...
-	 *		CKEDITOR.inline( 'content' );
-	 *
-	 * It is also possible to create an inline editor from the `<textarea>` element.
-	 * If you do so, an additional `<div>` element with editable content will be created
-	 * directly after the `<textarea>` element and the `<textarea>` element will be hidden.
-	 *
-	 * @param {Object/String} element The DOM element or its ID.
-	 * @param {Object} [instanceConfig] The specific configurations to apply to this editor instance.
-	 * See {@link CKEDITOR.config}.
-	 * @returns {CKEDITOR.editor} The editor instance created.
-	 */
+
 	CKEDITOR.inline = function( element, instanceConfig ) {
 		element = CKEDITOR.editor._getEditorElement( element );
 
@@ -156,11 +132,3 @@
 	} );
 } )();
 
-/**
- * Disables creating the inline editor automatically for elements with
- * the `contenteditable` attribute set to `true`.
- *
- *		CKEDITOR.disableAutoInline = true;
- *
- * @cfg {Boolean} [disableAutoInline=false]
- */
