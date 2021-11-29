aed923933e73b30a73b7f26ab299cc119127e1e4
Phạm Văn Minh Đức <55387191+minhducand2@users.noreply.github.com>
2021-11-29 20:51:47 +0700

    Update document.js

--- frontend-admin/src/assets/ckeditor/core/dom/document.js
+++ frontend-admin/src/assets/ckeditor/core/dom/document.js
@@ -1,23 +1,4 @@
-/**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
 
-/**
- * @fileOverview Defines the {@link CKEDITOR.dom.document} class which
- *		represents a DOM document.
- */
-
-/**
- * Represents a DOM document.
- *
- *		var document = new CKEDITOR.dom.document( document );
- *
- * @class
- * @extends CKEDITOR.dom.domObject
- * @constructor Creates a document class instance.
- * @param {Object} domDocument A native DOM document.
- */
 CKEDITOR.dom.document = function( domDocument ) {
 	CKEDITOR.dom.domObject.call( this, domDocument );
 };
@@ -76,15 +57,7 @@ CKEDITOR.tools.extend( CKEDITOR.dom.document.prototype, {
 		return styleSheet || style.$.sheet;
 	},
 
-	/**
-	 * Creates a {@link CKEDITOR.dom.element} instance in this document.
-	 *
-	 * @param {String} name The name of the element.
-	 * @param {Object} [attributesAndStyles]
-	 * @param {Object} [attributesAndStyles.attributes] Attributes that will be set.
-	 * @param {Object} [attributesAndStyles.styles] Styles that will be set.
-	 * @returns {CKEDITOR.dom.element}
-	 */
+
 	createElement: function( name, attribsAndStyles ) {
 		var element = new CKEDITOR.dom.element( name, this );
 
@@ -99,12 +72,7 @@ CKEDITOR.tools.extend( CKEDITOR.dom.document.prototype, {
 		return element;
 	},
 
-	/**
-	 * Creates a {@link CKEDITOR.dom.text} instance in this document.
-	 *
-	 * @param {String} text Value of the text node.
-	 * @returns {CKEDITOR.dom.element}
-	 */
+
 	createText: function( text ) {
 		return new CKEDITOR.dom.text( text, this );
 	},
@@ -116,16 +84,7 @@ CKEDITOR.tools.extend( CKEDITOR.dom.document.prototype, {
 		this.getWindow().focus();
 	},
 
-	/**
-	 * Returns the element that is currently designated as the active element in the document.
-	 *
-	 * **Note:** Only one element can be active at a time in a document.
-	 * An active element does not necessarily have focus,
-	 * but an element with focus is always the active element in a document.
-	 *
-	 * @returns {CKEDITOR.dom.element} Active element or `null` if an IE8-9 bug is encountered.
-	 * See [#10030](https://dev.ckeditor.com/ticket/10030).
-	 */
+
 	getActive: function() {
 		var $active;
 		try {
@@ -136,26 +95,13 @@ CKEDITOR.tools.extend( CKEDITOR.dom.document.prototype, {
 		return new CKEDITOR.dom.element( $active );
 	},
 
-	/**
-	 * Gets an element based on its ID.
-	 *
-	 *		var element = CKEDITOR.document.getById( 'myElement' );
-	 *		alert( element.getId() ); // 'myElement'
-	 *
-	 * @param {String} elementId The element ID.
-	 * @returns {CKEDITOR.dom.element} The element instance, or `null` if not found.
-	 */
+
 	getById: function( elementId ) {
 		var $ = this.$.getElementById( elementId );
 		return $ ? new CKEDITOR.dom.element( $ ) : null;
 	},
 
-	/**
-	 * Gets a node based on its address. See {@link CKEDITOR.dom.node#getAddress}.
-	 *
-	 * @param {Array} address
-	 * @param {Boolean} [normalized=false]
-	 */
+
 	getByAddress: function( address, normalized ) {
 		var $ = this.$.documentElement;
 
@@ -187,26 +133,14 @@ CKEDITOR.tools.extend( CKEDITOR.dom.document.prototype, {
 		return $ ? new CKEDITOR.dom.node( $ ) : null;
 	},
 
-	/**
-	 * Gets elements list based on a given tag name.
-	 *
-	 * @param {String} tagName The element tag name.
-	 * @returns {CKEDITOR.dom.nodeList} The nodes list.
-	 */
+
 	getElementsByTag: function( tagName, namespace ) {
 		if ( !( CKEDITOR.env.ie && ( document.documentMode <= 8 ) ) && namespace )
 			tagName = namespace + ':' + tagName;
 		return new CKEDITOR.dom.nodeList( this.$.getElementsByTagName( tagName ) );
 	},
 
-	/**
-	 * Gets the `<head>` element for this document.
-	 *
-	 *		var element = CKEDITOR.document.getHead();
-	 *		alert( element.getName() ); // 'head'
-	 *
-	 * @returns {CKEDITOR.dom.element} The `<head>` element.
-	 */
+
 	getHead: function() {
 		var head = this.$.getElementsByTagName( 'head' )[ 0 ];
 		if ( !head )
@@ -247,20 +181,7 @@ CKEDITOR.tools.extend( CKEDITOR.dom.document.prototype, {
 		return new CKEDITOR.dom.window( this.$.parentWindow || this.$.defaultView );
 	},
 
-	/**
-	 * Defines the document content through `document.write`. Note that the
-	 * previous document content will be lost (cleaned).
-	 *
-	 *		document.write(
-	 *			'<html>' +
-	 *				'<head><title>Sample Document</title></head>' +
-	 *				'<body>Document content created by code.</body>' +
-	 *			'</html>'
-	 *		);
-	 *
-	 * @since 3.5.0
-	 * @param {String} html The HTML defining the document content.
-	 */
+	
 	write: function( html ) {
 		// Don't leave any history log in IE. (https://dev.ckeditor.com/ticket/5657)
 		this.$.open( 'text/html', 'replace' );
@@ -277,41 +198,18 @@ CKEDITOR.tools.extend( CKEDITOR.dom.document.prototype, {
 		this.$.close();
 	},
 
-	/**
-	 * Wrapper for `querySelectorAll`. Returns a list of elements within this document that match
-	 * the specified `selector`.
-	 *
-	 * **Note:** The returned list is not a live collection (like the result of native `querySelectorAll`).
-	 *
-	 * @since 4.3.0
-	 * @param {String} selector A valid [CSS selector](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors).
-	 * @returns {CKEDITOR.dom.nodeList}
-	 */
 	find: function( selector ) {
 		return new CKEDITOR.dom.nodeList( this.$.querySelectorAll( selector ) );
 	},
 
-	/**
-	 * Wrapper for `querySelector`. Returns the first element within this document that matches
-	 * the specified `selector`.
-	 *
-	 * @since 4.3.0
-	 * @param {String} selector A valid [CSS selector](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors).
-	 * @returns {CKEDITOR.dom.element}
-	 */
+
 	findOne: function( selector ) {
 		var el = this.$.querySelector( selector );
 
 		return el ? new CKEDITOR.dom.element( el ) : null;
 	},
 
-	/**
-	 * Internet Explorer 8 only method. It returns a document fragment which has all HTML5 elements enabled.
-	 *
-	 * @since 4.3.0
-	 * @private
-	 * @returns DocumentFragment
-	 */
+
 	_getHtml5ShivFrag: function() {
 		var $frag = this.getCustomData( 'html5ShivFrag' );
 
